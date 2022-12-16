d91587280ea44f8b5870c3b099f59d6522e06475
Searcherr <alfafemida@gmail.com>
2022-12-16 08:10:50 +0100

    Prototype Pattern - success

--- Java Patterns in Python/Prototype_success.py
+++ Java Patterns in Python/Prototype_success.py
@@ -18,7 +18,7 @@ class Address:
         return f"{self.country}, {self.city}, {self.street}"
 
 
-class Student:
+class Student(StudentPrototype):
     def __init__(self, first_name, last_name, address):
         self.first_name = first_name
         self.last_name = last_name
@@ -27,11 +27,20 @@ class Student:
     def __str__(self):
         return f"{self.first_name} {self.last_name} lives at {self.address}"
 
+    def clone(self):
+        return copy.deepcopy(self)
+
 
 maria = Student('Maria', 'Teresa', Address('Poland', 'Warsaw', 'Jana Pawla II'))
 print(maria)
 print('---------------')
-franz = maria
+franz = maria.clone()
 franz.first_name = 'Franz'
 franz.last_name = 'Stephan'
 print(franz, '\n', maria)
+
+print(maria.__dict__)
+print(franz.__dict__)
+
+print(hash(maria))
+print(hash(franz))
