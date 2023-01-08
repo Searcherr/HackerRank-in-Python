cdb96db14d6a490d2a2243c9c0bc5899fd80e71a
searcherr <alfafemida@gmail.com>
2023-01-08 10:43:04 +0100

    ShortcutApp - start coding

--- ShortcutsApp/main.py
+++ ShortcutsApp/main.py
@@ -1,12 +1,27 @@
 class Template:
-    def __init__(self, message, shortcut, file_path):
+    def __init__(self, message, shortcut, path_to_file):
         self.message = message
         self.shortcut = shortcut
-        self.file_path = file_path
+        self.path_to_file = path_to_file
 
-    def read_file(self):
-        pass
+    def __str__(self):
+        return f"{self.message}, {self.shortcut}, {self.path_to_file}"
+
+    def read_file(self, path_to_file):
+        with open(self.path_to_file) as file:
+            lines = [line.rstrip() for line in file]
+        return lines
 
     def make_shortcut(self):
         pass
+
+if __name__ == '__main__':
+    user_file = '/Users/dmytror/Documents/test_files.Text1.tct'
+    user_template = Template()
+    user_template.file_path = user_file
+
+    print(user_template.__dict__)
+
+
+
 
