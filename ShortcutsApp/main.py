bd72032b27f5eecacb9585d3235d8f8cd76e422e
searcherr <alfafemida@gmail.com>
2023-01-08 10:17:17 +0100

    ShortcutApp - start coding

---
+++ ShortcutsApp/main.py
@@ -0,0 +1,12 @@
+class Template:
+    def __init__(self, message, shortcut, file_path):
+        self.message = message
+        self.shortcut = shortcut
+        self.file_path = file_path
+
+    def read_file(self):
+        pass
+
+    def make_shortcut(self):
+        pass
+
