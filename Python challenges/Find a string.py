10ed264fd3272f55a00b56d4ff02f47a1d9d5723
Searcherr <alfafemida@gmail.com>
2023-01-11 20:31:11 +0100

    Find sub_string in a string

---
+++ Python challenges/Find a string.py
@@ -0,0 +1,21 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string) - len(sub_string)+1):
+        if string[i] == sub_string[0]:
+            is_in = True
+            for j in range(len(sub_string)):
+                if string[i+j] != sub_string[j]:
+                    is_in = False
+            if is_in == True:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input('Input your text: ').strip()
+    sub_string = input('').strip()
+    print(string, sub_string)
+
+
+    count = count_substring(string, sub_string)
+    print(count)
