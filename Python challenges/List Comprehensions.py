56dda3452c8444ec50501a954d73abf916b65d24
Searcherr <alfafemida@gmail.com>
2023-01-26 10:48:33 +0100

    New Python challenge

--- Python challenges/List Comprehensions.py
+++ Python challenges/List Comprehensions.py
@@ -1,7 +1,12 @@
-"""
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-"""
+    x = int(input("Enter the value of x:"))
+    y = int(input("Enter the value of y:"))
+    z = int(input("Enter the value of z:"))
+    n = int(input("Enter the value of n:"))
+
+    all_possible_combinations = [[i, j, k] 
+                                for i in range(x + 1) 
+                                for j in range(y + 1) 
+                                for k in range(z + 1) 
+                                if i + j + k != n]
+    print(all_possible_combinations)
